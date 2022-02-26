30d1c2662ffb81b5085ebab6126fae63cd84436d
Matheus Tirabassi <tirabassi.matheus@aluno.ifsp.edu.br>
2022-02-26 14:05:57 -0300

    feat: Cria página de edição de item
    Signed-off-by: Matheus Tirabassi <tirabassi.matheus@aluno.ifsp.edu.br>

--- react-migration/src/Routes.tsx
+++ react-migration/src/Routes.tsx
@@ -1,7 +1,7 @@
 import Footer from "components/Footer"
-import NavBar from "components/NavBar"
 import AdminHome from "pages/Admin"
 import Contact from "pages/Contact"
+import Form from "pages/Form"
 import Home from "pages/Home"
 import Login from "pages/Login"
 import NoMatch from "pages/NoMatch"
@@ -29,6 +29,9 @@ const HomeRoutes = () => {
 				<Route path={ROUTES.CONTACT} element={<Contact />} />
 				<Route path={ROUTES.ADMIN} element={<AdminHome />} />
 				<Route path={ROUTES.NOMATCH} element={<NoMatch />} />
+				<Route path={`${ROUTES.ADMIN}${ROUTES.FORM}${ROUTES.PIZZA}`}>
+					<Route path=":productId" element={<Form />} />
+				</Route>
 			</Routes>
 			<Footer />
 		</BrowserRouter>
